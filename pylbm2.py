c5a962699b9ad606018f358025282609010df747
Ted Tower <tedonarrival@gmail.com>
2022-01-14 09:46:38 -0600

    pylbm2

--- pylbm2.py
+++ pylbm2.py
@@ -16,52 +16,56 @@ class LBM():
         self.omega=omega
         
         # init velocity, density, and distribution fields
-        self.v=np.zeros((*sizeyx,2))
-        self.rho=np.ones(sizeyx)
+        self.fields={'v':np.zeros((*sizeyx,2)),
+                     'rho':np.ones(sizeyx),
+                     'solid':np.zeros(sizeyx),
+                     'Fin':np.zeros((*sizeyx,self.ndir)),
+                     'Fout':np.zeros((*sizeyx,self.ndir)),
+                     'Feq':np.zeros((*sizeyx,self.ndir))}
         self.initDistribution();
         
-        self.solid=np.zeros(sizeyx)  # solid points for bouncebacko documentation available 
+        #self.solid=np.zeros(sizeyx)  # solid points for bouncebacko documentation available 
         self.toreflect=[0,1,2,3,4,5,6,7,8]
         self.reflected=[0,3,4,1,2,7,8,5,6]
-        self.bounced=self.F.copy()
+        self.bounced=self.fields['Fin'].copy()
     
     def initDistribution(self):
-        self.Feq=np.zeros((*self.dim,self.ndir))
+        self.fields['Feq']=np.zeros((*self.dim,self.ndir))
         self.calcFeq();
-        self.F=self.Feq.copy()
+        self.fields['Fin']=self.fields['Feq'].copy()
         
     def calcFeq(self):
         #c_squ=1/3;
-        u2c=1.5*(self.v[:,:,0]**2+self.v[:,:,1]**2);
+        u2c=1.5*(self.fields['v']**2).sum(axis=-1)
         for ii in range(self.ndir):
-            cuns=3*(self.c[0,ii]*self.v[:,:,0]+self.c[1,ii]*self.v[:,:,1])#/c_squ
-            self.Feq[:,:,ii]=self.w[ii]*self.rho*(1+cuns+0.5*cuns**2-u2c)
+            cuns=3*(self.c[0,ii]*self.fields['v'][:,:,0]+self.c[1,ii]*self.fields['v'][:,:,1])
+            self.fields['Feq'][:,:,ii]=self.w[ii]*self.fields['rho']*(1+cuns+0.5*cuns**2-u2c)
                 
     def calcMacro(self):
-        self.rho=self.F.sum(axis=-1)
+        F=self.fields['Fin']
+        self.fields['rho']=F.sum(axis=-1)
         with np.errstate(invalid='ignore'):
-            self.v[:,:,0]=((self.F[:,:,1]+self.F[:,:,5]+self.F[:,:,8])-
-                          (self.F[:,:,3]+self.F[:,:,6]+self.F[:,:,7]))/self.rho
-            self.v[:,:,1]=((self.F[:,:,2]+self.F[:,:,5]+self.F[:,:,6])-
-                          (self.F[:,:,4]+self.F[:,:,7]+self.F[:,:,8]))/self.rho
+            self.fields['v'][:,:,0]=((F[...,1]+F[...,5]+F[...,8])-
+                                     (F[...,3]+F[...,6]+F[...,7]))/self.fields['rho']
+            self.fields['v'][:,:,1]=((F[...,2]+F[...,5]+F[...,6])-
+                                     (F[...,4]+F[...,7]+F[...,8]))/self.fields['rho']
     def stream(self):
         for ii in range(self.ndir):
-            self.F[:,:,ii]=np.roll(self.F[:,:,ii],
-                                   (self.c[1,ii],self.c[0,ii]),
-                                   axis=(0,1))
+            self.fields['Fin'][...,ii]=np.roll(self.fields['Fin'][:,:,ii],
+                                   (self.c[1,ii],self.c[0,ii]),axis=(0,1))
     def collide(self):
-        self.F=self.omega*self.Feq+(1-self.omega)*self.F;
+        self.fields['Fout']=self.omega*self.fields['Feq']+(1-self.omega)*self.fields['Fin'];
     def bounceback(self,ON):
         for dd in range(self.ndir):
-            F=self.F[:,:,dd]
+            F=self.fields['Fout'][:,:,dd]
             B=self.bounced[:,:,self.reflected[dd]]
             F[ON]=B[ON]
-            self.F[:,:,dd]=F#self.bounced[:,:,self.reflected[dd]]
+            self.fields['Fout'][:,:,dd]=F#self.bounced[:,:,self.reflected[dd]]
     def sim(self,steps=10,callbacks=None,verbose=False):
         if(callbacks is None): callbacks={}
         for k in ['postStream','postMacro']:
             if(k not in callbacks): callbacks[k]=[]
-        ON=np.where(self.solid)
+        ON=np.where(self.fields['solid'])
         t0=time.time();
         tepoch=t0
         for ii in range(steps):
@@ -69,12 +73,12 @@ class LBM():
             if((ii>0) and (ii%100==0)):
                 tnow=time.time()
                 telapsed=tnow-t0
-                mlups=np.prod(self.rho.shape)*ii/1e6/telapsed
+                mlups=np.prod(self.fields['rho'].shape)*ii/1e6/telapsed
                 print('%d: %.3gmlups (%.1fsec/epoch)'%(ii,mlups,tnow-tepoch))
                 tepoch=tnow
             self.stream()
             for dd in range(self.ndir):
-                self.bounced[:,:,dd]=self.F[:,:,self.toreflect[dd]]
+                self.bounced[...,dd]=self.fields['Fin'][...,self.toreflect[dd]]
             # callbacks (e.g. vel BCs, report out)
             for cb in callbacks['postStream']:
                 cb(self)
@@ -82,14 +86,14 @@ class LBM():
             for cb in callbacks['postMacro']:
                 cb(self)
             # BC: solids bounceback
-            self.rho[ON]=0;
+            self.fields['rho'][ON]=0;
             for jj in [0,1]:
-                v0=self.v[:,:,jj];v0[ON]=0;self.v[:,:,jj]=v0
+                v0=self.fields['v'][:,:,jj];v0[ON]=0;self.fields['v'][:,:,jj]=v0
             
             self.calcFeq();
             self.collide()
             self.bounceback(ON);
-            if(np.any(self.rho>10)):
+            if(np.any(self.fields['rho']>10)):
                 print('ack!: velocity too high! (step %d)'%self.step)
                 break
         if(verbose):
@@ -98,8 +102,9 @@ class LBM():
 if(__name__=='__main__'):
     import matplotlib.pyplot as plt
     S=LBM((30,60))
-    S.solid[10:20,15:20]=1
+    S.fields['solid'][10:20,15:20]=1
     def cb_vel(self):
-        self.v[:,0,1]=.1
-    S.sim(steps=500,callbacks=[cb_vel])
-    plt.imshow(S.v[:,:,1])
+        self.fields['v'][:,0,1]=.1
+    S.sim(steps=500,callbacks={'postMacro':[cb_vel]})
+    plt.imshow(S.fields['v'][:,:,0])
+    plt.plot(S.fields['v'][15,:,0])
